603c05602e61e53aca01219d6cdcd19eac59eda8
saurabh singh patel <saurabh955920@gmail.com>
2024-06-30 12:55:44 +0530

    add pre installation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -49,7 +49,7 @@ export default function Home() {
   return (
     <div>
       {/* top */}
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-5xl mx-auto'>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
           Find your next <span className='text-slate-500'>perfect</span>
           <br />
@@ -98,7 +98,8 @@ export default function Home() {
             </div>
             <div className='flex flex-wrap gap-4'>
               {offerListings.map((listing) => (
-                <ListingItem listing={listing} key={listing._id} />
+                <ListingItem listing={listing} key={listing._id}
+                />
               ))}
             </div>
           </div>
@@ -111,7 +112,8 @@ export default function Home() {
             </div>
             <div className='flex flex-wrap gap-4'>
               {rentListings.map((listing) => (
-                <ListingItem listing={listing} key={listing._id} />
+                <ListingItem listing={listing} key={listing._id}
+                />
               ))}
             </div>
           </div>
